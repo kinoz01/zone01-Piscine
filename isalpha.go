ef612a4b92a0f7bf5854b157a67d6b73323ba413
kino <kino@pop-os.localdomain>
2024-03-11 15:24:42 +0100

    (quest 5) is it alpha

---
+++ isalpha.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func IsAlpha(s string) bool {
+	sliceS:=[]rune(s)
+	for i:=0; i<len(sliceS); i++{
+		if sliceS[i]>='A' && sliceS[i]<='Z' || sliceS[i]>='a' && sliceS[i]<='z' || sliceS[i]>='0' && sliceS[i]<='9'{
+			continue
+		} else {
+			return false
+		}
+	}
+	return true
+}
+
+func main() {
+	fmt.Println(IsAlpha("Hello! How are you?"))
+	fmt.Println(IsAlpha("HelloHowareyou"))
+	fmt.Println(IsAlpha("What's this 4?"))
+	fmt.Println(IsAlpha("Whatsthis4"))
+
+}
