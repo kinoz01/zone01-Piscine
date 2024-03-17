7df679b87b691f9d99c8e2cd45c963ec15db49cf
kino <kino@pop-os.localdomain>
2024-03-17 17:59:45 +0100

    (quest 9) func param

--- countif.go
+++ countif.go
@@ -4,7 +4,7 @@ import "fmt"
 
 func CountIf(f func(string) bool, tab []string) int {
 	count := 0
-	for _, str := range tab{
+	for _, str := range tab {
 		if f(str) {
 			count++
 		}
@@ -14,7 +14,7 @@ func CountIf(f func(string) bool, tab []string) int {
 
 func main() {
 	tab1 := []string{"Hello", "how", "are", "you"}
-	tab2 := []string{"This","1", "is", "4", "you"}
+	tab2 := []string{"This", "1", "is", "4", "you"}
 	answer1 := CountIf(IsNumeric, tab1)
 	answer2 := CountIf(IsNumeric, tab2)
 	fmt.Println(answer1)
