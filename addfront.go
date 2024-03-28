af33b3ae421696191363279ee3095f7264dd39cd
kino <kino@pop-os.localdomain>
2024-03-28 16:51:48 +0100

    (checkpoint) add cell in the front of an array

--- addfront.go
+++ addfront.go
@@ -4,16 +4,16 @@ import "fmt"
 
 func AddFront(s string, slice []string) []string {
 	r := []string{s}
-	for _, str :=range slice{
+	for _, str := range slice {
 		r = append(r, str)
-	} 
+	}
 	return r
 }
 
 func main() {
-    fmt.Println(AddFront("Hello", []string{"world"}))
-    fmt.Println(AddFront("Hello", []string{"world", "!"}))
-    fmt.Println(AddFront("Hello", []string{}))
+	fmt.Println(AddFront("Hello", []string{"world"}))
+	fmt.Println(AddFront("Hello", []string{"world", "!"}))
+	fmt.Println(AddFront("Hello", []string{}))
 }
 
 // checkpoint
