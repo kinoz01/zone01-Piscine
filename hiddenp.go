2c61938e5a87a4dd637b6992f2e7cf6891d8a8a4
kino <kino@pop-os.localdomain>
2024-03-29 07:02:35 +0100

    (checkpoint) string hidden in a string

---
+++ hiddenp.go
@@ -0,0 +1,32 @@
+package main
+
+import "os"
+
+func main() {
+	if len(os.Args[1:]) != 2 {
+		return
+	}
+	s1 := os.Args[1]
+	s2 := os.Args[2]
+	if len(s2) < len(s1) || len(s1) == 0 {
+		os.Stdout.WriteString("0\n")
+		return
+	}
+	j := 0
+	for i := 0; i < len(s1); i++ {
+		for j < len(s2) {
+			if s1[i] == s2[j] {
+				j++
+				break
+			}
+			j++
+		}
+		if j == len(s2) {
+			os.Stdout.WriteString("0\n")
+			return
+		}
+	}
+	os.Stdout.WriteString("1\n")
+}
+
+// checkpoint
